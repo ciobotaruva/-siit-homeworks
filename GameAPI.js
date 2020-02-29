6d28edf10f9e9d2bcf22885311c762e3765bd02a
Ciobotaru Valentin <Ciobotaruva@gmail.com>
2020-02-29 22:45:25 +0200

    Please enter the commit message for your changes. Lines starting

--- GameAPI.js
+++ GameAPI.js
@@ -15,34 +15,52 @@ function displayGames(games) {
     document.body.append(fragment);
 }
 
+function createHtml(game) {
+    const gameWrapper = document.createElement('article');
 
+    //create the html elements
+    const image = document.createElement('img');
+    const title = document.createElement('h3');
+    const description = document.createElement('p');
+    const deleteBtn = document.createElement('button');
+
+
+    //insert the content in the html
+
+    image.src = game.imageUrl;
+    title.innerHTML = game.title;
+    description.innerHTML = game.description;
+
+    gameWrapper.classList.add('all-games');
+
+
+    gameWrapper.append(image, title, description);
+    return gameWrapper;
+
+}
 //Old code
-// function createHtml(game) {
-//     const gameWrapper = document.createElement('article');
-
-//     //create the html elements
-//     const image = document.createElement('img');
-//     const title = document.createElement('h3');
-//     const description = document.createElement('p');
-//     const deleteBtn = document.createElement('button');
+// async function getGamesData() {
+//     const response = await fetch('https://games-world.herokuapp.com/games');
+//     const games = await response.json();
 
 
-//     //insert the content in the html
 
-//     image.src = game.imageUrl;
-//     title.innerHTML = game.title;
-//     description.innerHTML = game.description;
-
-//     gameWrapper.classList.add('all-games');
-
-
-//     gameWrapper.append(image, title, description);
-//     return gameWrapper;
-
+//     for (const game of games) {
+//         const gameImg = document.createElement('img');
+//         const gameTitle = document.createElement('h4');
+//         const gameDescription = document.createElement('p');
+//         const gameSelector = document.querySelector('.all-games');
+//         gameImg.src = game.imageUrl;
+//         gameTitle.innerHTML = game.title;
+//         gameDescription.innerHTML = game.description;
+//         gameSelector.append(gameImg);
+//         gameSelector.append(gameTitle);
+//         gameSelector.append(gameDescription);
+//     }
 // }
 
-
-
 // getGamesData();
+
+getGamesData();
 
 
